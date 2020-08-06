ff08e70b62ed4a0657fa8e32486d2deeb91bf15a
roylau <roy-lau@foxmail.com>
2020-08-06 16:21:42 +0800

    wechat-official-accounts：调通点击回复

--- wechat-official-accounts/wechat/reply.js
+++ wechat-official-accounts/wechat/reply.js
@@ -4,12 +4,34 @@
 const request = require('request-promise'),
     config = require('../config')
 
+const features = `<a href="weixin://bizmsgmenu?msgmenucontent=日期查询&msgmenuid=1">1.日期查询</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=列车查询&msgmenuid=2">2.列车查询</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=数字计算&msgmenuid=3">3.数字计算</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=快递查询&msgmenuid=4">4.快递查询</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=菜谱大全&msgmenuid=5">5.菜谱大全</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=天气查询&msgmenuid=6">6.天气查询</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=图片搜索&msgmenuid=7">7.图片搜索</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=星座运势&msgmenuid=8">8.星座运势</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=新闻资讯&msgmenuid=9">9.新闻资讯</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=成语接龙&msgmenuid=10">10.成语接龙</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=故事大全&msgmenuid=11">11.故事大全</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=聊天对话&msgmenuid=12">12.聊天对话</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=笑话大全&msgmenuid=13">13.笑话大全</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=城市邮编&msgmenuid=14">14.城市邮编</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=顺口溜&msgmenuid=15">15.顺口溜</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=绕口令&msgmenuid=16">16.绕口令</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=歇后语&msgmenuid=17">17.歇后语</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=脑筋急转弯&msgmenuid=18">18.脑筋急转弯</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=中英互译&msgmenuid=19">19.中英互译</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=获取菜单&msgmenuid=20">20.获取菜单</a>
+<a href="weixin://bizmsgmenu?msgmenucontent=更新菜单&msgmenuid=21">21.更新菜单</a>`
+
 const tip = '终于等到你~ 还好没放弃~ \n' +
     '你就是我等待千年的那只…… /坏笑\n' +
-    '是不是觉得我很聪明/疑问 no？ 我有以下这些能力呢！\n' +
-    '日期查询\n列车查询\n数字计算\n快递查询\n菜谱大全\n天气查询\n图片搜索\n星座运势\n新闻资讯\n成语接龙\n故事大全\n聊天对话\n笑话大全\n城市邮编\n顺口溜\n绕口令\n歇后语\n股票查询\n汽油报价\n脑筋急转弯\n附近酒店\n中英互译'
+    '是不是觉得我很聪明/疑问 /No？ \n' +
+    '手机上点击 <a href="weixin://bizmsgmenu?msgmenucontent=服务&msgmenuid=0">服务</a> 看看我的能力吧！'
 
-const sayHi = '你好呀！/坏笑'
+const sayHelp = '我可以为您提供以下服务：\n' + features
 
 
 module.exports = async (ctx, next) => {
@@ -83,8 +105,8 @@ module.exports = async (ctx, next) => {
             }
 
             ctx.body = menuMsg
-        } else if (message.Content === 'hi' || message.Content === 'hello') {
-            ctx.body = sayHi + `<a href="https://${ctx.host}/test/fetchUserList">获取用户列表</a>`
+        } else if ( /1|hi|hello|服务/.test(message.Content) ) {
+            ctx.body = sayHelp
         } else {
             /*
               调用图灵机器人处理文本消息
